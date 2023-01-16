4d473bf59299f31f797ef3b3216f356c6f8dc787
harshal <harshalp2120@gmail.com>
2023-01-16 17:16:55 +0530

    Page source getrefresh key implemented

--- app/src/main/java/com/app/chatbenchmarkapp/ui/paging/ChatsPagingSource.kt
+++ app/src/main/java/com/app/chatbenchmarkapp/ui/paging/ChatsPagingSource.kt
@@ -13,7 +13,12 @@ class ChatsPagingSource(
     private val TAG = "ChatsPagingSource"
 
     override fun getRefreshKey(state: PagingState<Int, Chat>): Int? {
-        return null
+        return state.anchorPosition?.let { anchorPos ->
+            val page1 = state.closestPageToPosition(anchorPos)?.prevKey?.plus(1)
+            val page2 = state.closestPageToPosition(anchorPos)?.nextKey?.minus(1)
+            Log.d(TAG, "getRefreshKey: Anchor pos: $anchorPos, Page 1: $page1, page 2: $page2")
+            page1 ?: page2
+        }
     }
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Chat> {
